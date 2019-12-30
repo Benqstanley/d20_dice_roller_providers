47300b43a0a9c779f61448d6abc6e14f716c4028
Benjamin Quentin Stanley <zianderthalapps@Benjamins-Mac-mini.local>
2019-12-30 17:05:42 -0500

    adding some files to git repository

---
+++ android/app/src/main/kotlin/com/zianderthalapps/d20_dice_roller/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.zianderthalapps.d20_dice_roller
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
