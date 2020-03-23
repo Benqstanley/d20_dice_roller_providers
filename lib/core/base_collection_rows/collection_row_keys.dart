1d653ac905ebcb35b22b190233afcf0ccf24a019
Benjamin Quentin Stanley <zianderthalapps@Benjamins-Mac-mini.local>
2020-03-23 04:30:25 -0400

    increased coverage

--- lib/core/base_collection_rows/collection_row_keys.dart
+++ lib/core/base_collection_rows/collection_row_keys.dart
@@ -3,6 +3,4 @@ import 'package:flutter/material.dart';
 class CollectionRowKeys{
   static Key numberOfDiceTextField = Key('numberOfDiceTextField');
   static Key diceTypeDropDown = Key('diceTypeDropDown');
-  static Key namedCollectionRow = Key('namedCollectionRow');
-  static Key namedMultiCollectionRow = Key('namedMultiCollectionRow');
 }
